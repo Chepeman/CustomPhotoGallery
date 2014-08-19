8c33797cce92f2339fff3023d46b4cd7621e6f02
JCarlosCS <jcortes@itexico.net>
2014-08-19 10:49:22 -0500

    Added the activity to add photos from camera.

--- app/src/main/java/com/customphotogallery/jcarlos/customphotogallery/GalleryActivity.java
+++ app/src/main/java/com/customphotogallery/jcarlos/customphotogallery/GalleryActivity.java
@@ -1,23 +1,42 @@
 package com.customphotogallery.jcarlos.customphotogallery;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 
 public class GalleryActivity extends Activity {
-
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int MEDIA_TYPE_VIDEO = 2;
     private ScrollView scrollGallery;
+    private Uri fileUri;
+    private ArrayList<View> sections;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
         scrollGallery = (ScrollView) findViewById(R.id.scrollGallery);
+        sections = new ArrayList<View>();
         addSection("First Section");
     }
 
@@ -46,15 +65,86 @@ public class GalleryActivity extends Activity {
         }
     }
 
-    void addSection(String message){
+    void addSection(String message) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View v = inflater.inflate(R.layout.section_layout, null);
+        TextView sectionMessage = (TextView) v.findViewById(R.id.sectionTitle);
+        sectionMessage.setText(message);
+        sections.add(v);
+        scrollGallery.addView(v);
+    }
+
+    void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(fileUri.getPath(), 200, 200);
+                Intent intent = new Intent(this,PhotoActivity.class);
+                intent.putExtra("image", bit_map);
+                startActivity(intent);
+                //addImageToTheGrid(bit_map);
+
+            } else if (resultCode == RESULT_CANCELED) {
+                //El usuario cancela la imagen a capturar
+            } else {
+                //La captura de la imagen fallo.
+            }
+        }
+    }
+
+    /*private void addImageToTheGrid(Bitmap bit_map) {
         LayoutInflater inflater = LayoutInflater.from(this);
         View v = inflater.inflate(R.layout.section_layout, null);
         TextView sectionMessage = (TextView) v.findViewById(R.id.sectionTitle);
         sectionMessage.setText(message);
         scrollGallery.addView(v);
+    }*/
+
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
     }
 
-    void openCamera(){
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
 
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CustomPhotoGallery");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
+
 }
+
+
